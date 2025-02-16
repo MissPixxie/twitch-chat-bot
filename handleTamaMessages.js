df8bba0f19f06a6bc3076b4448d27459e996caef
Emelie Hansson <emeliehanssonn@hotmail.se>
2025-02-16 20:34:37 +0100

    cleaned up some code

--- handleTamaMessages.js
+++ handleTamaMessages.js
@@ -3,19 +3,10 @@
 //
 import { sendChatMessage } from "./chatBot.js";
 
-let health;
-
 class TamaSocket {
 	constructor(io) {
 		this.io = io;
 		this.createConnection();
-		this.latestHealth = {
-			love: 0,
-			hunger: 0,
-			fun: 0,
-			bath: 0,
-			sleep: 0,
-		};
 		this.OAuthToken = null;
 	}
 	createConnection() {
@@ -25,52 +16,28 @@ class TamaSocket {
 			console.log("A client connected ");
 			this.socket = socket;
 			socket.on("messageFromTama", (newMessage) => {
+				console.log(newMessage);
 				this.updateHealth(newMessage);
 			});
 		});
 	}
 
 	updateHealth(newHealth) {
-		for (let [key, value] of Object.entries(newHealth)) {
-			if (this.latestHealth.hasOwnProperty(key)) {
-				this.latestHealth[key] = value;
-			}
-		}
-		console.log(this.latestHealth);
 		let timmysHealth = "";
-		for (let [key, value] of Object.entries(this.latestHealth)) {
+		for (let [key, value] of Object.entries(newHealth)) {
 			timmysHealth += ` ${key}: ${value}`;
 		}
-		sendChatMessage(`Timmy's health is ${timmysHealth}`, this.OAuthToken);
-	}
-
-	availableCommands(command) {
-		console.log("available commands function" + command);
-		const commands = [
-			"!love",
-			"!feed",
-			"!play",
-			"!bath",
-			"!sleep",
-			"!health",
-		];
-
-		const findCommand = commands.indexOf(command);
-		if (findCommand !== -1) {
-			return true;
-		} else {
-			return false;
-		}
+		sendChatMessage(
+			`Timmy's health is ${timmysHealth} 🔋`,
+			this.OAuthToken
+		);
 	}
 
 	sendMessage(message, chatter, OAuthToken) {
 		this.OAuthToken = OAuthToken;
-		if (message === "!health") {
-			this.io.emit("botMessage", message);
-		} else {
-			this.io.emit("botMessage", message);
-			let answer = this.returnMessage(message, chatter);
-			return answer;
+		this.io.emit("botMessage", message);
+		if (message !== "!health") {
+			return this.returnMessage(message, chatter);
 		}
 	}
 
@@ -93,9 +60,6 @@ class TamaSocket {
 			case "!love":
 				answer = `@${chatter} gave Timmy a hug ❤️`;
 				break;
-			case "!health":
-				answer = `Timmy's health is....${this.latestHealth} `;
-				break;
 			case "!timmycommands":
 				answer = this.getCommands();
 				break;
@@ -112,26 +76,6 @@ class TamaSocket {
 			" !health - Get Timmy's current health 🔋",
 		];
 	}
-	// sendMessage(message, chatter) {
-	// 	if (message === "!health") {
-	// 		this.io.emit("botMessage", message, (response) => {
-	// 			health = response;
-	// 			console.log(response);
-	// 		});
-	// 	} else {
-	// 		this.io.emit("botMessage", message);
-	// 		console.log(message);
-	// 	}
-	// 	let answer = returnMessage(message, chatter);
-	// 	return answer;
-	// }
-	// checkHealth(socket) {
-	// 	socket.on("messageFromTama", (newMessage) => {
-	// 		health = newMessage;
-	// 		console.log(health);
-	// 		//currentHealth();
-	// 	});
-	// }
 }
 
 // class Message {
@@ -151,43 +95,3 @@ class TamaSocket {
 // }
 
 export { TamaSocket };
-
-// function getCommands() {
-// 	return [
-// 		" !feed - Feed Timmy 🍔",
-// 		" !bath - Give Timmy a bath 🛁",
-// 		" !sleep - Make Timmy take a nap 💤",
-// 		" !play - Play with Timmy ⚽",
-// 		" !love - Give Timmy some love ❤️",
-// 		" !health - Get Timmy's current health 🔋",
-// 	];
-// }
-
-// function returnMessage(message, chatter) {
-// 	let answer;
-
-// 	switch (message) {
-// 		case "!feed":
-// 			answer = `@${chatter} fed Timmy 🍔`;
-// 			break;
-// 		case "!bath":
-// 			answer = `Timmy was stinky so @${chatter} made him take a bath 🛁`;
-// 			break;
-// 		case "!sleep":
-// 			answer = `@${chatter} told a story about their life and Timmy fell asleep 💤`;
-// 			break;
-// 		case "!play":
-// 			answer = `@${chatter} played a game with Timmy ⚽`;
-// 			break;
-// 		case "!love":
-// 			answer = `@${chatter} gave Timmy a hug ❤️`;
-// 			break;
-// 		case "!health":
-// 			answer = `Timmy's health is....${health} `;
-// 			break;
-// 		case "!timmycommands":
-// 			answer = getCommands();
-// 			break;
-// 	}
-// 	return answer;
-// }
